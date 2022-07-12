90fa0659c370610e2d109f81147c9c4e9ae2b14e
Amr Megahed <amr.megahed@bakerysoft.net>
2022-07-12 18:52:50 +0200

    fixed controller name

--- src/Http/Controllers/BakerySoftController.php
+++ src/Http/Controllers/BakerySoftController.php
@@ -2,6 +2,7 @@
 
 namespace BakerySoft\LaravelBakerySoft\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use BakerySoft\LaravelBakerySoft\Http\Classes\BSModelProcess;
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-$namespace = 'BakerySoft\LaravelBakerySoft\Http\Controllers';
+use BakerySoft\LaravelBakerySoft\Http\Controllers\BakerySoftController;
 
-Route::namespace($namespace)->group(function () {
-  Route::get('bakery-soft', [BakerySoftController::class, 'index']);
-  Route::get('bakery-soft/install/packages', [BakerySoftController::class, 'packages'])->name('packages');
-  Route::get('bakery-soft/generate-crud', [BakerySoftController::class, 'loadCrud'])->name('load.crud');
-  Route::post('bakery-soft/generate', [BakerySoftController::class, 'generate'])->name('generate');
-  Route::post('bakery-soft/install/laravel/ui', [BakerySoftController::class, 'installUi'])->name('install-ui');
-  Route::post('bakery-soft/install/excel', [BakerySoftController::class, 'installExcel'])->name('install-excel');
-  Route::post('bakery-soft/install/image', [BakerySoftController::class, 'installImage'])->name('install-image');
-  Route::post('bakery-soft/install/realrashid', [BakerySoftController::class, 'installRealrashid'])->name('install-realrashid');
-  Route::post('bakery-soft/install/yajra', [BakerySoftController::class, 'installYajra'])->name('install-yajra');
-  Route::post('bakery-soft/install/laratrust', [BakerySoftController::class, 'installLaratrust'])->name('install-laratrust');
-});
+Route::get('bakery-soft', [BakerySoftController::class, 'index']);
+Route::get('bakery-soft/install/packages', [BakerySoftController::class, 'packages'])->name('packages');
+Route::get('bakery-soft/generate-crud', [BakerySoftController::class, 'loadCrud'])->name('load.crud');
+Route::post('bakery-soft/generate', [BakerySoftController::class, 'generate'])->name('generate');
+Route::post('bakery-soft/install/laravel/ui', [BakerySoftController::class, 'installUi'])->name('install-ui');
+Route::post('bakery-soft/install/excel', [BakerySoftController::class, 'installExcel'])->name('install-excel');
+Route::post('bakery-soft/install/image', [BakerySoftController::class, 'installImage'])->name('install-image');
+Route::post('bakery-soft/install/realrashid', [BakerySoftController::class, 'installRealrashid'])->name('install-realrashid');
+Route::post('bakery-soft/install/yajra', [BakerySoftController::class, 'installYajra'])->name('install-yajra');
+Route::post('bakery-soft/install/laratrust', [BakerySoftController::class, 'installLaratrust'])->name('install-laratrust');
