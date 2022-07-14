32e1d53f39d44fc35b068e2fe69f84c90e6e09a6
Amr Megahed <amr.megahed@bakerysoft.net>
2022-07-14 21:55:34 +0200

    update package

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ use BakerySoft\LaravelBakerySoft\Http\Controllers\BakerySoftController;
 Route::get('bakerysoft', [BakerySoftController::class, 'index']);
 Route::get('bakerysoft/install/packages', [BakerySoftController::class, 'packages'])->name('packages');
 Route::get('bakerysoft/generate-crud', [BakerySoftController::class, 'loadCrud'])->name('load.crud');
-Route::post('bakerysoft/generate', [BakerySoftController::class, 'generate'])->name('generate')
+Route::post('bakerysoft/generate', [BakerySoftController::class, 'generate'])->name('generate');
 Route::post('bakerysoft/install/laravel/ui', [BakerySoftController::class, 'installUi'])->name('install-ui');
 Route::post('bakerysoft/install/excel', [BakerySoftController::class, 'installExcel'])->name('install-excel');
 Route::post('bakerysoft/install/image', [BakerySoftController::class, 'installImage'])->name('install-image');
