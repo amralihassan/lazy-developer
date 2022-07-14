0de1636cc23431ce779d521e0223dd4eb9f8fd7c
Amr Megahed <amr.megahed@bakerysoft.net>
2022-07-14 22:08:12 +0200

    update package

--- src/Http/Controllers/BakerySoftController.php
+++ src/Http/Controllers/BakerySoftController.php
@@ -85,6 +85,7 @@ class BakerySoftController extends Controller
     public function installUi()
     {
         shell_exec('cd .. && ' . $this->composerRequire . config('packages.ui'));
+        sleep(20);
         shell_exec('cd .. && ' .  config('packages.auth'));
 
         // add dashboard Controller
